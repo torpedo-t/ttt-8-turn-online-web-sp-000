a4080f876efe559734eaf1ac236f27ac0b6a43e4
Thomas John Helin <thomashelin@yahoo.com>
2020-03-19 19:53:23 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,36 @@
+def turn(board)
+ puts "Please enter 1-9:"
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+ if index.between?(0, 8) && !position_taken?(board, index)
+   return true
+ end
+end
+
+def position_taken?(board, index)
+ if board[index] == " " || board[index] == "" || board[index] == nil
+  return false
+else
+  return true
+  end
+ end
+
+ def input_to_index(number)
+   number.to_i
+   converted_input = (number.to_i - 1)
+ end
+
+ board = [" ", " ", " "]
+
+ def move(board, number, value="X")
+  board[number] = value
+ end
